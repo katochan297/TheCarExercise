7899bbb31130511b12b29ee009af34a746a04b53
katochan297 <thuantranduc168@gmail.com>
2016-10-07 14:17:19 +0800

    update

--- TheCar/TheCar/Service/CarService.cs
+++ TheCar/TheCar/Service/CarService.cs
@@ -4,23 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TheCar.Enum;
-using TheCar.Provider;
 
 namespace TheCar.Service
 {
     public class CarService
     {
-        private readonly TaxRateProvider _taxProvider;
+        private static Dictionary<Region, List<TaxRate>> _dicTaxRate;
 
         public CarService()
         {
-            _taxProvider = new TaxRateProvider();
+            _dicTaxRate = new Dictionary<Region, List<TaxRate>>()
+            {
+                {
+                    Region.Europe, new List<TaxRate>
+                    {
+                        new TaxRate {Min = -1, Max = 2.0f, Rate = 100},
+                        new TaxRate {Min = 2.0f, Max = 5.0f, Rate = 120},
+                        new TaxRate {Min = 5.0f, Max = 1000.0f, Rate = 200}
+                    }
+                },
+                {
+                    Region.USA, new List<TaxRate>
+                    {
+                        new TaxRate {Min = -1, Max = 2.0f, Rate = 75},
+                        new TaxRate {Min = 2.0f, Max = 5.0f, Rate = 90},
+                        new TaxRate {Min = 5.0f, Max = 1000.0f, Rate = 150}
+                    }
+                },
+                {
+                    Region.Japan, new List<TaxRate>
+                    {
+                        new TaxRate {Min = -1, Max = 2.0f, Rate = 70},
+                        new TaxRate {Min = 2.0f, Max = 5.0f, Rate = 80},
+                        new TaxRate {Min = 5.0f, Max = 1000.0f, Rate = 135}
+                    }
+                },
+                {
+                    Region.Other, new List<TaxRate>
+                    {
+                        new TaxRate {Min = -1, Max = 1000.0f, Rate = 0}
+                    }
+                }
+            };
+        }
+
+        private float GetTaxRate(Car car)
+        {
+            if (_dicTaxRate.ContainsKey(car.OriginalRegion))
+            {
+                TaxRate taxRate =
+                    _dicTaxRate[car.OriginalRegion].FirstOrDefault(i => car.Liter > i.Min && car.Liter <= i.Max && car.Liter >= 0);
+
+                try
+                {
+                    return taxRate.GetRate();
+                }
+                catch (NullReferenceException)
+                {
+                    //write log
+                    throw;
+                }
+
+            }
+
+            return 0;
         }
 
         private float CaculateImportTaxPrice(Car car)
         {
-            return _taxProvider.CaculateImportTax(car);
+            return car.OriginalPrice * GetTaxRate(car);
         }
+
 
 
         public float CaculateEndUserPrice_USD(Car car)
--- TheCar/TheCar/Service/TaxRate.cs
+++ TheCar/TheCar/Service/TaxRate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheCar.Provider
+namespace TheCar.Service
 {
     internal class TaxRate
     {
